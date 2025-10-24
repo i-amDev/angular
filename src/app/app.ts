2afebe465406383109040a836bd3cd56bdbfdf22
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-24 12:01:23 +0530

    - Learned about two way binding in Angular. - Imported "FormsModule" in the ts file and used "ngModel" in the html file to bind the data from ts to html and vice versa.

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import {Component2} from './component2/component2';
 import {IfElseComponent} from './if-else-component/if-else-component';
 import {SwitchComponent} from './switch-component/switch-component';
 import {ForLoopComponent} from './for-loop-component/for-loop-component';
+import {TwoWayBindingComponent} from './two-way-binding-component/two-way-binding-component';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Component2,IfElseComponent, SwitchComponent, ForLoopComponent],
+  imports: [Login, Component2,IfElseComponent, SwitchComponent, ForLoopComponent, TwoWayBindingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
